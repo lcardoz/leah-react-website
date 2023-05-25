2b9555a158a7716a263dc3a329cc5786bc25552f
Leah Cardoz <leahcardoz@gmail.com>
2023-05-24 23:09:10 -0400

    styled about me section

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ function App() {
       </h1>
       <div id="about-me-container">
         <div id ="about-me">
-          <p style={{textDecoration: "underline"}}>A BIT ABOUT ME:</p>
-          <p>concert enthusiast, world traveler, student always</p>
-          <p style={{fontStyle: "italic"}}>~currently~</p>
+          {/* <p style={{textDecoration: "underline"}}>A BIT ABOUT ME:</p> */}
+          <p style={{fontSize: "20px"}}>software engineer + concert enthusiast + student always</p>
+          <p >~CURRENTLY~</p>
           <p>looking for: frontend or fullstack web development opportunities</p>
-          <p>living in: Brooklyn, NY</p>
           <p>listening to: <a href="https://open.spotify.com/album/4FR8Z6TvIsC56NLyNomNRE?si=5zkRDp48T-OWHAAyrIAx9g">Stop Making Sense (Live)</a> by Talking Heads</p>
+          <p>living in: Brooklyn, NY</p>
         </div>
       </div>
+      <div>
+        <h3>pic of me</h3>
+      </div>
       <footer>
-        <h3>contact info</h3>
+        <h3>links to pages: projects | resumes | contact links</h3>
       </footer>
     </>
   );
