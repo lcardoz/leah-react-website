6fdc385258946d3a0b8229dbb1f3094aa69124a3
Leah Cardoz <leahcardoz@gmail.com>
2023-05-24 22:51:09 -0400

    built out about me section

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div id="app">
-      <header id="app-header">
-        <h1 id="title">
-          <span id="small-header">welcome to the personal site of  </span>
-          <strong>LEAH CARDOZ</strong>
-        </h1>
-      </header>
-      <div id ="main">
-        <p>info here</p>
+    <>
+      <h1 id="title">
+        <span id="small-header">welcome to the personal site of  </span>
+        <strong>LEAH CARDOZ</strong>
+      </h1>
+      <div id="about-me-container">
+        <div id ="about-me">
+          <p style={{textDecoration: "underline"}}>A BIT ABOUT ME:</p>
+          <p>concert enthusiast, world traveler, student always</p>
+          <p style={{fontStyle: "italic"}}>~currently~</p>
+          <p>looking for: frontend or fullstack web development opportunities</p>
+          <p>living in: Brooklyn, NY</p>
+          <p>listening to: <a href="https://open.spotify.com/album/4FR8Z6TvIsC56NLyNomNRE?si=5zkRDp48T-OWHAAyrIAx9g">Stop Making Sense (Live)</a> by Talking Heads</p>
+        </div>
       </div>
       <footer>
-        <h3>info here</h3>
+        <h3>contact info</h3>
       </footer>
-    </div>
+    </>
   );
 }
 
