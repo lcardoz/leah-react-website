9795d1f7c43d8414fa819d01c92bcf3e4d9b1cc5
Leah Cardoz <leahcardoz@gmail.com>
2023-06-07 21:26:05 -0400

    added gif to nopage component

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Route path = '/portfolio' element={ <div style={{height: "calc(100vh - 111px)", overflowY: "scroll", overflowX: "hidden"}}><Portfolio /></div>} />
         <Route path = '/resumes' element={ <div style={{height: "calc(100vh - 111px)", overflowY: "scroll", overflowX: "hidden"}}><Resumes /></div>} />
         <Route path = '/contact' element={ <div style={{height: "calc(100vh - 111px)", overflowY: "scroll", overflowX: "hidden"}}><Contact /></div>} />
-        <Route path = '*' element={ <NoPage />} />
+        <Route path = '*' element={ <div style={{height: "calc(100vh - 111px)", overflowY: "scroll", overflowX: "hidden"}}><NoPage /></div>} />
         <Route path = '/' element={ <div style={{height: "calc(100vh - 111px)", overflowY: "scroll", overflowX: "hidden"}}> <Home /> </div>} />
       </Routes>
       <ContactLinks/>
--- src/components/NoPage.js
+++ src/components/NoPage.js
@@ -3,8 +3,12 @@ import React from 'react';
 const NoPage = () => {
   return (
     <div style={{display: 'flex', justifyContent: 'center', borderRadius: "30px"}}>
-      <div style={{color: 'white', backgroundColor: '#fc3903', fontWeight: '700', textAlign: 'center', margin: '15px', width: '40%', padding: '75px 50px 75px 50px', borderRadius: "30px", fontSize: '20px'}}>
-        Oops! This page does not exist.
+      <div style={{color: 'white', backgroundColor: '#fc3903', fontWeight: '700', textAlign: 'center', margin: '15px', width: '50%', padding: '25px 50px 50px 50px', borderRadius: "30px", fontSize: '20px',}}>
+        <h2>Oops! This page does not exist.</h2>
+        <br />
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <img src="https://media.giphy.com/media/xT0BKFZgaGq6af8hZ6/giphy.gif" alt="David Byrne dancing" style={{maxWidth: '100%', maxHeight: '100%', width: 'auto', height: 'auto'}} />
+        </div>
       </div>
     </div>
   )
