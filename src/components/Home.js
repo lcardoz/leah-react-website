b68f23a57c8614a410217186c8a7a0b9c8670518
Leah Cardoz <leahcardoz@gmail.com>
2023-06-20 11:59:36 -0400

    updated looking for and listening to in about me and added learning

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,9 @@ const Home = () => {
         <div id ="about-me">
           <p style={{fontSize: "18px"}}>software engineer + concert enthusiast + student always</p>
           <p >~CURRENTLY~</p>
-          <p>looking for: frontend or fullstack web development opportunities</p>
-          <p>listening to: <a id="listening-to" href="https://open.spotify.com/album/4FR8Z6TvIsC56NLyNomNRE?si=5zkRDp48T-OWHAAyrIAx9g" target="_blank" rel="noopener noreferrer" alt="Stop-Making-Sense-Album">Stop Making Sense (Live)</a> by Talking Heads</p>
+          <p>looking for: full-time web development opportunities</p>
+          <p>learning: Redux, Data Structures & Algorithms </p>
+          <p>listening to: <a id="listening-to" href="https://open.spotify.com/album/31UtR7w5vJtg8AmBvWAwL5?si=IoTnEC1zQ7Ch6fI-D3NlwA" target="_blank" rel="noopener noreferrer" alt="Space-Heavy-Album">Space Heavy</a> by King Krule</p>
           <p>living in: Brooklyn, NY</p>
         </div>
       </div>
