1df7c11428be1a3dad9e250cefc3ed7c7acb3099
Leah Cardoz <leahcardoz@gmail.com>
2024-06-03 18:36:40 -0400

    updated current section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import leahPic from '../images/leahpic1.JPG';
+import React from "react";
+import leahPic from "../images/leahpic1.JPG";
 
 const Home = () => {
   return (
     <div>
       <div id="about-me-container">
-        <div id ="about-me">
-          <p style={{fontSize: "18px"}}>software engineer + concert enthusiast + student always</p>
-          <p >~CURRENTLY~</p>
-          <p>looking for: full-time web development opportunities</p>
-          <p>learning: Redux, Data Structures & Algorithms </p>
-          <p>listening to: <a id="listening-to" href="https://open.spotify.com/album/31UtR7w5vJtg8AmBvWAwL5?si=IoTnEC1zQ7Ch6fI-D3NlwA" target="_blank" rel="noopener noreferrer" alt="Space-Heavy-Album">Space Heavy</a> by King Krule</p>
+        <div id="about-me">
+          <p style={{ fontSize: "18px" }}>
+            <i>"a pleasure to have in class"</i>
+          </p>
+          <p>~CURRENTLY~</p>
+          <p>seeking: a new gig!</p>
           <p>living in: Brooklyn, NY</p>
+          <p>
+            listening to:{" "}
+            <a
+              id="listening-to"
+              href="https://open.spotify.com/album/2JdE3ilolUGhsNkW1oQfvf?si=43mZyxYjTDeUm9_FC_L_uw"
+              target="_blank"
+              rel="noopener noreferrer"
+              alt="Night-Reign-Album"
+            >
+              Night Reign
+            </a>{" "}
+            by Arooj Aftab
+          </p>
+          <p>reading: Walking in Clear Water in a Pool Painted Black by Cookie Mueller</p>
         </div>
       </div>
       <div id="leah-pic-container">
         <img id="leah-pic" src={leahPic} alt="Leah in Antigua" />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
